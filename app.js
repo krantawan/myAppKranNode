3e46892bcd5680b13c1fdf1dd3da1581db6b8fd1
Krantawan <krantawan@ku.th>
2021-12-04 10:34:04 +0700

    two commit

--- app.js
+++ app.js
@@ -5,9 +5,5 @@ app.get("/", function(req,res){
     res.send("Hello! KRANTAWAN")
 })
 
-app.get("/hello", function(req,res){
-    res.send("Hello World!")
-})
-
 app.listen(process.env.PORT || 5000)
 module.exports = app
